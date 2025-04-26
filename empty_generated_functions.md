1722e403395f03b1a299208b85f01814a581b835
Ugo Landini <ugo.landini@gmail.com>
2025-04-26 09:50:39 +0200

    updated functions page

---
+++ empty_generated_functions.md
@@ -0,0 +1,30 @@
+---
+# Title, summary, and page position.
+linktitle: Functions
+summary: Functions available in JR Templates
+weight: 110
+#icon: book
+icon_pack: fas
+
+# Page metadata.
+title: Functions
+date: '2023-04-17T00:00:00Z'
+type: book # Do not modify.
+---
+
+This section contains all the available functions in JR Templates.
+
+There are 10 different categories
+
+- [Address](#address-functions)
+- [Context](#context-functions)
+- [Date and Time](#date-and-time-functions)
+- [Finance](#finance-functions)
+- [Math](#math-functions)
+- [Network](#network-functions)
+- [People](#people-functions)
+- [Phone](#phone-functions)
+- [Text](#text-functions) 
+- [Utilities](#utilities-functions)
+
+You can look at this list via ```jr man``` command.
--- generate_md.sh
+++ generate_md.sh
@@ -1,10 +1,49 @@
-jr man -c network -m >> ~/Desktop/network.md
-jr man -c people -m >> ~/Desktop/people.md
-jr man -c phone -m >> ~/Desktop/phone.md
-jr man -c address -m >> ~/Desktop/address.md
-jr man -c utilities -m >> ~/Desktop/utilities.md
-jr man -c context -m >> ~/Desktop/context.md
-jr man -c math -m >> ~/Desktop/math.md
-jr man -c finance -m >> ~/Desktop/finance.md
-jr man -c text -m >> ~/Desktop/text.md
-jr man -c time -m >> ~/Desktop/time.md
+echo "" >> generated_functions.md
+echo "## Address functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c address -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Context functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c context -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Date and Time functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c time -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Finance functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c finance -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Math functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c math -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Network functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c network -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## People functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c people -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Phone functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c phone -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Text functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c text -m >> generated_functions.md
+
+echo "" >> generated_functions.md
+echo "## Utilities functions" >> generated_functions.md
+echo "" >> generated_functions.md
+jr man -c utilities -m >> generated_functions.md
